a9e32e5f9dd302e9c143d03b097f2db58c7f9269
Ashwini <ashwiniparaye9309@gmail.com>
2023-01-19 15:40:45 +0530

    createListing page added

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { collection, getDocs, query, where, orderBy, limit, startAfter } from 'firebase/firestore';
+// import { useParams } from 'react-router-dom';
+import { collection, getDocs, query, where, orderBy, limit } from 'firebase/firestore';
 import { db } from '../firebase.config';
 import { toast } from 'react-toastify';
 import Spinner from '../components/Spinner';
@@ -13,7 +13,7 @@ function Offers() {
     const [ listings, setListings ] = useState(null)
     const [ loading, setLoading ] = useState(true)
 
-    const params = useParams()
+    // const params = useParams()
 
     //fetching listing from firebase
     useEffect(() => {
